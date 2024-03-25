003abbc6a010064da6970630f48f52d4415ca9eb
Premraj18 <premrajprasad18@gmail.com>
2024-03-25 11:30:25 +0530

    frontend part complete a basic ui design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import { Button, Container } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
+
+//pages
+import UserPage from './pages/UserPage'
+import PostPage from './pages/PostPage'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <Container maxWidth='620px'>
+      <Header/>
+      <Routes>
+        <Route path='/:username' element={<UserPage/>}/>
+        <Route path='/:username/post/:pid' element={<PostPage/>}/>
+      </Routes>
+     </Container>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/UserPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import UserHeader from '../components/UserHeader'
+import UserPost from '../components/UserPost'
+
+const UserPage = () => {
+  return (
+    <>
+      <UserHeader/>
+      <UserPost likes={1200} replies={481} postImg='/post1.png' postTitle="Let's talk about threads."/>
+      <UserPost likes={452} replies={12} postImg='/post2.png' postTitle="Nice tutorial"/>
+      <UserPost likes={321} replies={98} postImg='/post3.png' postTitle="I love this guy"/>
+      <UserPost likes={212} replies={56} postTitle="This is my first thread"/>
+    </>
+  )
+}
+
+export default UserPage
